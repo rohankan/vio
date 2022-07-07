e8a0ea88ac802825bf115c12c494e0c79d69dbd4
Rohan <rohankanchana@github.com>
2022-07-07 10:33:23 -0700

    vio

---
+++ vio.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+command="$1"
+storage="<path to storage.sh>"
+
+if [ -z "$command" ]; then
+	echo "No command specified."
+	exit 1
+fi
+
+if [ "$command" = "export" ] || [ "$command" = "e" ]; then 
+	directory=$(readlink -f "$2")
+	shorthand="$3"
+	if [ -z "$directory" ] || [ -z "$shorthand" ]; then 
+		echo "To export, specify both a directory and shorthand."
+		exit 1
+	fi
+	if [ "$shorthand" = "export" ] || [ "$shorthand" = "e" ]; then
+		echo "Cannot use reserved keywords for shorthand."
+		exit 1
+	fi
+	echo "export $shorthand=$directory;" >> "$storage";
+else
+	shorthand="$2"
+	source "$storage"; cd "${!$shorthand}"
+	vim `fzf`
+fi
+
